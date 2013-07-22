65856614a7c18d4ec69dc031faf8b93a8fb1b154
Tom MacWright <tom@macwright.org>
2013-07-22 15:18:09 -0400

    Add render script

---
+++ scripts/render.js
@@ -0,0 +1,50 @@
+var Canvas = require('canvas'),
+    glob = require('glob'),
+    fs = require('fs'),
+    rainbowDash = require('rainbow-dash'),
+    leftpad = require('leftpad'),
+    width = 3000,
+    height = 3000,
+    c = new Canvas(width, height),
+    ctx = c.getContext('2d');
+
+var pos = glob.sync('data/v0/bus/positions/*.geojson').map(function(p) {
+    return [p.match(/(\d+).geojson/)[1], JSON.parse(fs.readFileSync(p))];
+});
+
+ctx.fillStyle = '#000';
+ctx.fillRect(0, 0, width, height);
+
+var bbox = [
+    [-77.191, 39.055],
+    [-76.808, 38.780]
+];
+
+var hexOut = rainbowDash({ outputFormat: 'hex' }, {
+    0: '#00ffff',
+    1: '#ff11ff'
+});
+
+var gwidth = bbox[1][0] - bbox[0][0];
+var gheight = bbox[1][1] - bbox[0][1];
+
+function proj(p) {
+    return [
+        ((p.geometry.coordinates[0] - bbox[0][0]) / gwidth) * width,
+        ((p.geometry.coordinates[1] - bbox[0][1]) / gheight) * height];
+}
+
+var x = 0, j = 0;
+
+pos.forEach(function(p, i) {
+    ctx.fillStyle = hexOut(i / pos.length);
+    p[1].features.forEach(function(_) {
+        var pt = proj(_);
+        ctx.fillRect(pt[0], pt[1], 1, 1);
+    });
+    // if ((j++ % 10) === 0) {
+    //     fs.writeFileSync('frames/' + leftpad(x++, 10) + '.png', c.toBuffer());
+    // }
+});
+fs.writeFileSync('big.png', c.toBuffer());
+
